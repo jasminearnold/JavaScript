20150efc0b059522186fd0fc02dcb3329e88165d
jasminearnold <jasmine.arnold@me.com>
2022-12-23 21:07:14 +0900

    Commit1

---
+++ index.js
@@ -0,0 +1,10 @@
+var myName= "Jasmine";
+var myHeight= "165";
+var myCountry = "USA";
+
+function myFunction(_myName, _myHeight, _myCountry){
+   console.log(myName, myHeight, myCountry);
+   return alert (myName + "" + myHeight + "" + myCountry)
+} 
+
+myFunction (myCountry ,myHeight, myCountry);
